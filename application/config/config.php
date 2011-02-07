e88177e6399a8953c3ed3f57a33fbbaf55ca18ce
yterajima <terra@e2esound.com>
2011-02-08 01:14:36 +0900

    Change Base Settings
    Edit config.php&database.php
    database.php had ignored that has database settings&password
    Add 2controllers

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'KLEvfTJ9dWU6OtflaSmeuWmZJWb1fuT7';
 
 /*
 |--------------------------------------------------------------------------
