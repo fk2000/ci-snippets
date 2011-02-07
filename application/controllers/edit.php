0e70226f2425ee6ed2f97126fa05fcbc0805590b
yterajima <terra@e2esound.com>
2011-02-08 03:34:26 +0900

    Create Edit Page

--- application/controllers/edit.php
+++ application/controllers/edit.php
@@ -4,11 +4,41 @@ class Edit extends CI_Controller
 	function __construct()
 	{
 		parent::__construct();
+
+		// Load Library and helper
+		$this->load->library("form_validation");
+		$this->load->helper("form");
 	}
 
 	function index()
 	{
-		$this->load->view('edit_view');
+		// form settings
+		$data["form_title_attr"] = array('name'=> 'title',
+																'id' => 'title',
+																'value' => '',
+																'maxlength'=> '100',
+																'size' => '50');
+		$data["form_code_attr"] = array("name" => "code",
+															"id" => "code",
+															"value" => "",
+															"cols" => "90",
+															"rows" => "20");
+		$data["code_type_options"] = array("text" => "Plain Text",
+																			 "php" => "PHP",
+																			 "js"  => "JavaScript");
+		$data["ticket"] = md5(uniqid(mt_rand(), TRUE));
+
+		$this->load->view('edit_view', $data);
+	}
+
+	function confirm()
+	{
+
+	}
+
+	function update()
+	{
+
 	}
 }
 
--- application/views/edit_view.php
+++ application/views/edit_view.php
@@ -6,14 +6,26 @@
 	<meta name="description" content="">
 	<meta name="copyright" content="Yuya Terajima/e2esound.com">
 
-	<link rel="stylesheet" href="<?php echo site_url("css/style.css");?>">
+	<link rel="stylesheet" href="<?php echo base_url();?>css/style.css">
 </head>
 <body id="PageName">
 	<header>
-
+	<h1>CI-SNIPPETS</h1>
 	</header>
 	<div id="contents">
-	edit_view
+	<h2>Edit</h2>
+	<?php
+		echo form_open("edit/confirm"),
+			form_hidden("ticket", $ticket),
+			form_label("タイトル", "title"),
+			form_input($form_title_attr),
+			form_label("言語", "code_type"),
+			form_dropdown("code_type", $code_type_options, "text"),
+			form_label("コード", "code"),
+			form_textarea($form_code_attr),
+			form_submit("confirm", "確認"),
+			form_reset("reset", "リセット"),
+			form_close();?>
 	<!--/#contents--></div>
 	<footer>
 
