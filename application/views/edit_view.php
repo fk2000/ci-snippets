656140fa5c146860721f744472fca34a9da9934b
yterajima <terra@e2esound.com>
2011-02-08 03:34:26 +0900

    Create Edit Page

--- application/views/edit_view.php
+++ application/views/edit_view.php
@@ -10,10 +10,22 @@
 </head>
 <body id="PageName">
 	<header>
-
+	<h1>CI-SNIPPETS</h1>
 	</header>
 	<div id="contents">
-	edit_view
+	<h2>Edit</h2>
+	<?php
+		echo form_open("edit/confirm"),
+			form_hidden("ticket", $ticket),
+			form_label("タイトル", "title"),
+			form_input($form_title_attr),
+			form_label("言語", "code_type"),
+			form_dropdown("code_type", $code_type_options, "text"),
+			form_label("コード", "code"),
+			form_textarea($form_code_attr),
+			form_submit("confirm", "確認"),
+			form_reset("reset", "リセット"),
+			form_close();?>
 	<!--/#contents--></div>
 	<footer>
 
