bf4b09089ff04e9c8c06334584facf45ea094b68
yterajima <terra@e2esound.com>
2011-02-09 01:55:21 +0900

    Create model

---
+++ application/models/snippets_model.php
@@ -0,0 +1,33 @@
+<?php
+class Snippets_model extendsCI_Model
+{
+	function __construct()
+	{
+		parent::__construct();
+		$this->load->library('');
+		$this->load->helper('');
+	}
+
+	function Insert()
+	{
+		// code...
+	}
+
+	function update()
+	{
+
+	}
+
+	function delete()
+	{
+
+	}
+
+	function select()
+	{
+
+	}
+}
+
+/* End of file snippets_model.php */
+/* Location: ./system/application/models/snippets_model.php */
