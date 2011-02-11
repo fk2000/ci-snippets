69e673a633336b203c4f74d15d1c05ccfb95321d
yterajima <terra@e2esound.com>
2011-02-11 16:10:03 +0900

    Rename snippets to display in controller and view

--- application/views/display_view.php
+++ application/views/display_view.php
@@ -1,7 +1,7 @@
 <div id="contents">
 <?php foreach($list as $code):?>
 
-<?php echo anchor("snippets/code/" . $code["id"], "<h3 class='title " . $code["id"] ."'>" . $code["title"] . "</h3>", $code["title"]);?>
+<?php echo anchor("display/code/" . $code["id"], "<h3 class='title " . $code["id"] ."'>" . $code["title"] . "</h3>", $code["title"]);?>
 <?php endforeach;?>
 <div id="pagination">
 <?php echo $pager;?>
