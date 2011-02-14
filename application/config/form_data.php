b361657a833280bb847fc1e4038c1dfa2da6a3ce
yterajima <terra@e2esound.com>
2011-02-14 13:22:07 +0900

    Add filetype in form_data.php

--- application/config/form_data.php
+++ application/config/form_data.php
@@ -2,7 +2,14 @@
 $config['code_type_options'] = array(
 																		"text" => "Plain Text",
 																		"php" => "PHP",
-																		"js"  => "JavaScript"
+																		"js"  => "JavaScript",
+																		"ruby" => "Ruby",
+																		"shell" => "shell",
+																		"html" => "html",
+																		"xml" => "xml",
+																		"sql" => "sql",
+																		"css" => "css",
+																		"perl" => "Perl"
 																	);
 
 $config["code_type_selected"] = "text";
