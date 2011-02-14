5670a55f6d7b82fddd4b8f0ca2e7315636f54b5f
yterajima <terra@e2esound.com>
2011-02-14 16:42:14 +0900

    bug fix in displat.php

--- application/views/display_view.php
+++ application/views/display_view.php
@@ -1,9 +1,14 @@
 <div id="contents">
-<?php foreach($list as $code):?>
+<?php
+if(!empty($list)):
+foreach($list as $code):?>
 
-<?php echo anchor("display/code/" . $code["id"], "<h3 class='title " . $code["id"] ."'>[" . code_name($code["code_type"]) . "]" . $code["title"] . "</h3>", $code["title"]);?>
+<?php echo anchor("display/code/" . $code["id"], "<h3 class='title " . $code["id"] ."'>[" . code_name($code["code_type"]) . "] " . $code["title"] . "</h3>", $code["title"]);?>
 <?php endforeach;?>
 <!--/#contents--></div>
 <div id="pagination">
 <?php echo $pager;?>
+<?php else:?>
+<p>スニペットが登録されていません。</p>
+<?php endif;?>
 </div>
